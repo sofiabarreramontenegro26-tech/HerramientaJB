30b8563ef15753f5326d61992dd06ff7d53a7c97
yenysilvacamargo1020-lgtm <yenysilvacamargo1020@gmail.com>
2026-08-27 21:29:03 -0500

    feat: crear modelo MovimientoInventario

---
+++ app/Models/MovimientoInventario.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MovimientoInventario extends Model
+{
+    use HasFactory; 
+
+    protected $table = "movimiento_inventario";
+
+    protected $fillable = [
+        
+    ];
+
+    protected $casts = [
+        
+    ];
+}
