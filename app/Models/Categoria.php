a289374df59236411255d04ead2f1b533354bebb
Laura Barrera <sofiabarreramontenegro26@gmail.com>
2026-08-29 22:24:39 -0500

    Correcion del modelo categoria

--- app/Models/Categoria.php
+++ app/Models/Categoria.php
@@ -2,19 +2,20 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Categoria extends Model
 {
     use HasFactory;
 
-    protected $table = 'categorias';
-    protected $primaryKey = 'id_categoria';
+    protected $table = "categorias";
+
+    protected $primaryKey = "id_categoria";
 
     protected $fillable = [
-        'nombre',
-        'descripcion',
+        "nombre",
+        "descripcion"
     ];
 
     public function productos()
