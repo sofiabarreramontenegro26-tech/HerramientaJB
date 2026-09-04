35abf02cb7d0f96df26f08f7ef984bd3b38d3318
leonardo123garavito-pixel <leonardo123garavito@gmail.com>
2026-09-03 19:15:39 -0500

    feat: se crea el servicio RegistroConectividadService

--- app/Services/RegistroConectividadService.php
+++ app/Services/RegistroConectividadService.php
@@ -19,4 +19,19 @@ class RegistroConectividadService
     {
         return $this->registroConectividadRepository->getById($id);
     }
+
+    public function store(array $data)
+    {
+        return $this->registroConectividadRepository->create($data);
+    }
+
+    public function update(int $id, array $data)
+    {
+        return $this->registroConectividadRepository->update($id, $data);
+    }
+
+    public function destroy(int $id)
+    {
+        return $this->registroConectividadRepository->delete($id);
+    }
 }
