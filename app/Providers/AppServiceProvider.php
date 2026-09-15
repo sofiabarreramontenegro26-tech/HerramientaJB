b0470638b2ff4fe06bdff7cbf54516b8273ac9ac
Danna Salcedo <ds0179881@gmail.com>
2026-09-14 20:21:37 -0500

    Creacion de providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Interfaces\RolInterface;
+use App\Repositories\RolRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(
+            RolInterface::class,
+            RolRepository::class
+        );
     }
 
     /**
