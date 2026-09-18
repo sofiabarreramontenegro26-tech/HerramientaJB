867c0a3522a4bedd0baebf64945dd847cfaa3035
Danna Salcedo <ds0179881@gmail.com>
2026-09-17 20:54:33 -0500

    correciones

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,21 +4,45 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 // Interfaces
-use App\Interfaces\UsuarioInterface;
 use App\Interfaces\RolInterface;
-use App\Interfaces\HistorialActividadInterface;
 use App\Interfaces\CategoriaInterface;
 use App\Interfaces\ProveedorInterface;
+use App\Interfaces\UsuarioInterface;
+use App\Interfaces\HistorialActividadInterface;
 use App\Interfaces\ProductoInterface;
-
+use App\Interfaces\EntradaInterface;
+use App\Interfaces\MovimientoInventarioInterface;
+use App\Interfaces\ConfiguracionAlertaInterface;
+use App\Interfaces\AlertaInventarioInterface;
+use App\Interfaces\CatalogoBusquedaInterface;
+use App\Interfaces\ProductoFavoritoInterface;
+use App\Interfaces\CotizacionInterface;
+use App\Interfaces\VentaInterface;
+use App\Interfaces\MaquinaInterface;
+use App\Interfaces\HojaVidaInterface;
+use App\Interfaces\MantenimientoInterface;
+use App\Interfaces\RegistroConectividadInterface;
 
 // Repositorios
-use App\Repositories\UsuarioRepository;
 use App\Repositories\RolRepository;
-use App\Repositories\HistorialActividadRepository;
 use App\Repositories\CategoriaRepository;
 use App\Repositories\ProveedorRepository;
+use App\Repositories\UsuarioRepository;
+use App\Repositories\HistorialActividadRepository;
 use App\Repositories\ProductoRepository;
+use App\Repositories\EntradaRepository;
+use App\Repositories\MovimientoInventarioRepository;
+use App\Repositories\ConfiguracionAlertaRepository;
+use App\Repositories\AlertaInventarioRepository;
+use App\Repositories\CatalogoBusquedaRepository;
+use App\Repositories\ProductoFavoritoRepository;
+use App\Repositories\CotizacionRepository;
+use App\Repositories\VentaRepository;
+use App\Repositories\MaquinaRepository;
+use App\Repositories\HojaVidaRepository;
+use App\Repositories\MantenimientoRepository;
+use App\Repositories\RegistroConectividadRepository;
+
 
 
 class AppServiceProvider extends ServiceProvider
